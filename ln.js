c3f58b563a4496bf991ded0bc02f1f2329818576
AsiComoLoOye <arturo.garro@asicomolooye.com>
2014-05-15 08:36:09 -0600

    La nación cambió
    Se agrega un nuevo elemento que la Nación agregó a su sitio web.

--- ln.js
+++ ln.js
@@ -1,1 +1,1 @@
-$('#LNA_background, #LNA_contenidoTotal,#ADI_background, #ADI_contenidoTotal,#PER_background, #PER_contenidoTotal,#EFI_background, #EFI_contenidoTotal').remove();
+$('#LNA_background, #LNA_contenidoTotal,#ADI_background, #ADI_contenidoTotal,#PER_background, #PER_contenidoTotal,#EFI_background, #EFI_contenidoTotal, #LNA_background_paywall').remove();
